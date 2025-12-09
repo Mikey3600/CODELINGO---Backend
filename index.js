ceeb46a91b95e58f139acfae471a6b644c360209
MAYANK <24052419>
2025-12-09 17:35:21 +0530

    Fixed Railway binding

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const authRoutes = require('./routes/auth')
 
 dotenv.config()
 connectDB()
+
 const app = express()
+
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/auth', authRoutes)
-app.listen(process.env.PORT || 5000)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`)
+})
